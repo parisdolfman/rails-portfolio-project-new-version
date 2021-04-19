f5700a56a662a9753952a0d90a9a7393d0685f04
parisdolfman <parisdolfman@gmail.com>
2021-04-19 15:26:12 -0400

    Added validation

--- rails-portfolio-project-main/app/models/review.rb
+++ rails-portfolio-project-main/app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
     belongs_to :user
 
     validates_inclusion_of :rating, :in => 1..5
+    validates :body, presence: true, length: { maximum: 500 }
+    validates :title, presence: true
 end
