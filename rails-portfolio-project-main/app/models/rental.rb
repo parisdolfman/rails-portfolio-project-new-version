27a19670f6b32c053d48a6e485c616c7a120e0d4
parisdolfman <parisdolfman@gmail.com>
2021-04-19 13:38:12 -0400

    Working on show link on index page

--- rails-portfolio-project-main/app/models/rental.rb
+++ rails-portfolio-project-main/app/models/rental.rb
@@ -2,4 +2,11 @@ class Rental < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
 
+
+    def full_add
+        @rentals.each do |rental|
+            rental.join(" ")
+        end 
+    end 
+
 end
