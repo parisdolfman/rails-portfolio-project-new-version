cbb2f4f997219df63b684256a3dcc3bdb69ee9c2
parisdolfman <parisdolfman@gmail.com>
2021-04-19 15:33:05 -0400

    Added validations

--- rails-portfolio-project-main/app/models/rental.rb
+++ rails-portfolio-project-main/app/models/rental.rb
@@ -1,12 +1,10 @@
 class Rental < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
-
-
-    def full_add
-        @rentals.each do |rental|
-            rental.join(" ")
-        end 
-    end 
+    
+    validates :street_add, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :owner, presence: true
 
 end
