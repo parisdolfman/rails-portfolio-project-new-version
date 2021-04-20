c308ab2aad82752f15fabf15c418982baa2848d6
parisdolfman <parisdolfman@gmail.com>
2021-04-20 09:11:38 -0400

    Fixed syntax errr

--- rails-portfolio-project-main/app/models/review.rb
+++ rails-portfolio-project-main/app/models/review.rb
@@ -5,5 +5,4 @@ class Review < ApplicationRecord
     validates_inclusion_of :rating, :in => 1..5
     validates :body, presence: true, length: { maximum: 500 }
     validates :title, presence: true
-    validates 
 end
