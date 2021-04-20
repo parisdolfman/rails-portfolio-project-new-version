dee2467c5ccd27a1cd00a8409311d990889cfbe6
parisdolfman <parisdolfman@gmail.com>
2021-04-20 12:42:21 -0400

    Added in model method for no rating

--- rails-portfolio-project-main/app/models/review.rb
+++ rails-portfolio-project-main/app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ApplicationRecord
     validates_inclusion_of :rating, :in => 1..5
     validates :body, presence: true, length: { maximum: 500 }
     validates :title, presence: true
+
+    def zero_stars
+        5 - rating.to_i
+    end 
 end
