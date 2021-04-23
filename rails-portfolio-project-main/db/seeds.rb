3db43fe7d4305ac8aeafb594b46ca90720ea0398
parisdolfman <parisdolfman@gmail.com>
2021-04-23 10:22:47 -0400

    Added more seeds for testing

--- rails-portfolio-project-main/db/seeds.rb
+++ rails-portfolio-project-main/db/seeds.rb
@@ -8,8 +8,16 @@
 
 Rental.create(street_add: "1428 Elm Street", city: "Springwood", state: "Ohio", owner: "Fred D. Krueger")
 
+Rental.create(street_add: "112 Ocean Avenue", city: "Amityville", state: "NY", owner: "Butch DeFeo")
+
+Rental.create(street_add: "8601 N Thorne Ln SW", city: "Lakewood", state: "WA", owner: "Kirtland Cutter")
+
 User.create(username: "AAA", email: "test@test1.com", password: "123456")
 
-Review.create(title: "Weird Dreams, Nice Lawn", body: "Open kitchen space, large green lawn. Had very strange dreams while living here.", rating: 3, rental_id: 2, user_id: 1)
+Review.create(title: "Weird Dreams, Nice Lawn", body: "Open kitchen space, large green lawn. Had very strange dreams while living here.", rating: 3, rental_id: 1, user_id: 1)
+
+Review.create(title: "I loved it, husband didn't", body: "While I loved the quaint house, my husband was adamant that he didn't. (Something about voices). Great price for the location.", rating: 4, rental_id: 2, user_id: 1)
+
+Review.create(title: "Sprawling Estate", body: "Plenty of space. Takes a while to heat in the winter but overall loved the home.", rating: 5, rental_id: 3, user_id: 1)
 
 puts "data loaded success"
