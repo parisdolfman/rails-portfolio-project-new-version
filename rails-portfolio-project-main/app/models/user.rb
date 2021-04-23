adf99599b82de55069a265e265cc8b1c644d441b
parisdolfman <parisdolfman@gmail.com>
2021-04-23 16:54:46 -0400

    Added for OnmiAuth

--- rails-portfolio-project-main/app/models/user.rb
+++ rails-portfolio-project-main/app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :rentals, through: :reviews
 
   validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
+  
+  devise :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    name_split = auth.info.name.split(" ")
+    user = User.where(email: auth.info.email).first
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
+      user
+  end
 end
