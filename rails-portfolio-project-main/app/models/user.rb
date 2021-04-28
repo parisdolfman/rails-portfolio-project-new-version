7837769b35eb3af3a2d6f877cf31e5a970aa393b
parisdolfman <parisdolfman@gmail.com>
2021-04-28 15:38:49 -0400

    Trying to add omniauth

--- rails-portfolio-project-main/app/models/user.rb
+++ rails-portfolio-project-main/app/models/user.rb
@@ -9,14 +9,13 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
   
-  #devise :omniauthable, omniauth_providers: %i[facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+     # user.name = auth.info.name   # assuming the user model has a name
+     # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
--- rails-portfolio-project-main/config/initializers/omniauth.rb
+++ rails-portfolio-project-main/config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']
+    provider :developer unless Rails.env.production?
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
