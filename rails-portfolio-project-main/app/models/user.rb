706e1b6d1461cf35ceb4e19cf309e1e25d3488f4
parisdolfman <parisdolfman@gmail.com>
2021-05-01 11:17:17 -0400

    Fixing Omniauth

--- rails-portfolio-project-main/app/models/user.rb
+++ rails-portfolio-project-main/app/models/user.rb
@@ -11,26 +11,22 @@ class User < ApplicationRecord
   
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-     # user.name = auth.info.name   # assuming the user model has a name
-     # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-    end 
-    end
-   
-    def self.new_with_session(params, session)
-      super.tap do |user|
-        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-          user.email = data["email"] if user.email.blank?
+    name_split = auth.info.name.split(" ")
+    user = User.where(email: auth.info.email).first
+    user ||=  auth.info.email.blank? ? User.create!(provider: auth.provider, uid: auth.uid, username: name_split[0], email: "test@test5.com", password: Devise.friendly_token[0, 20]) : nil 
+      user
+  end
+     
+      def self.new_with_session(params, session)
+        super.tap do |user|
+          if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+            user.email = data["email"] if user.email.blank?
+          end
         end
       end
-    end
-
   
-
-
-end
+    
+  
+  
+  end
+  
